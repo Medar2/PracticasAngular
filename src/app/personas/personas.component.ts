ebb92f4b2db0998f8b155926049747adad3e5e9e
Jose Caraballo <jcaraballo74@hotmail.com>
2021-09-18 16:10:12 -0400

    Init Commit

---
+++ src/app/personas/personas.component.ts
@@ -0,0 +1,13 @@
+
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-personas',
+    templateUrl: './personas.component.html',
+    // template: '<h1>Listado de Personas</h1><app-persona></app-persona><app-persona></app-persona>', //InLine
+    // styleUrls: ['./personas.component.css'
+    styles:['h1{color:blue;}']
+})
+
+export class PersonasComponent{}
