74d3567af26211145235e43ab43e9f499d58f983
Jose Caraballo <jcaraballo74@hotmail.com>
2021-09-18 17:41:54 -0400

    [add] Event Binding

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -5,11 +5,16 @@ import { Component } from '@angular/core';
 @Component({
     selector:'app-personas',
     templateUrl: './personas.component.html',
-    // template: '<h1>Listado de Personas</h1><app-persona></app-persona><app-persona></app-persona>', //InLine
-    // styleUrls: ['./personas.component.css'
-    styles:['h1{color:blue;}']
+    styleUrls: ['./personas.component.css']
 })
 
 export class PersonasComponent{
     deshabilitar= false;
+
+    mensaje = 'No se ha agregado ninguna persona';
+    
+    agregarPersona(){
+        this.mensaje = 'Persona agregada';
+    
+    }
 }
